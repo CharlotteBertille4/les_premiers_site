970051477f055e1f67ec54d7de04b57a9bd2c9bf
TOMENOU Charlotte <charlottetomenou@gmail.com>
2025-05-26 14:12:02 +0100

    feat: mise en place des différentes pages du site

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+// Bootstrap JS (modaux, dropdowns, etc.)
+import 'bootstrap';
+
+// Bootstrap CSS si tu ne passes pas par SCSS
+import './styles/app.scss';  // ou app.scss si tu utilises Sass
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -2,18 +2,40 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PublicController extends AbstractController
 {
-    #[Route('/public', name: 'app_public')]
-    public function index(): JsonResponse
+    #[Route('/', name: 'home')]
+    public function home(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/PublicController.php',
-        ]);
+        return $this->render('public/index.html.twig');
+    }
+
+    #[Route('/a-propos', name: 'a_propos')]
+    public function aPropos(): Response
+    {
+        return $this->render('public/a_propos.html.twig');
+    }
+
+    #[Route('/formations', name: 'formations')]
+    public function formations(): Response
+    {
+        return $this->render('public/formations.html.twig');
+    }
+
+    #[Route('/galerie', name: 'galerie')]
+    public function galerie(): Response
+    {
+        return $this->render('public/galerie.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('public/contact.html.twig');
     }
 }
